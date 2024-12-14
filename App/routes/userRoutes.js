7f52ddd690523e2776647e4cd51d4ede9aec38bb
Charlene Lara <140762985+Grazie01@users.noreply.github.com>
2024-12-15 07:27:25 +0800

    Update userRoutes.js

--- App/routes/userRoutes.js
+++ App/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const userRouter = express.Router();
-const {loginUser, getUser} = require('../controllers/StudentController')
+const { loginUser, getUser } = require(path.resolve(__dirname, '../controllers/StudentController')); 
 
 userRouter.post('/login', loginUser);
 userRouter.get('/get-student/:studentId', getUser)
