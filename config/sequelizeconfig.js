03aeced5f557ffbeb9b3a4af05899846620e20a2
Charlene Lara <140762985+Grazie01@users.noreply.github.com>
2024-12-14 08:17:34 +0800

    Update sequelizeconfig.js

--- config/sequelizeconfig.js
+++ config/sequelizeconfig.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
     development: {
       username: 'root',
       password: '',
@@ -6,19 +6,14 @@ module.exports = {
       host: 'localhost',
       dialect: 'mysql',
     },
-};
+};*/
 
-/**
- * module.exports = {
-    development: {
-      username: 'admin',
-      password: 'Revassist@123',
-      database: 'revassist',
-      host: '127.0.0.1 ',
-      dialect: 'mysql',
-    },
+module.exports = {
+  development: {
+    username: 'admin',
+    password: 'Revassist@123',
+    database: 'revassist',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
 };
-  
- * 
- */
-  
