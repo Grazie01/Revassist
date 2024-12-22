796752f7c8f011749b782cc81a489d45bfe4096e
Charlene Lara <140762985+Grazie01@users.noreply.github.com>
2024-12-22 16:30:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -36,9 +36,10 @@ var corsOptions = {
 app.use((req, res, next) => {
     const startTime = Date.now()
     //console.log('Request Method:', req.method);
-    console.log('Request Headers:', req.headers);
+    //console.log('Request Headers:', req.headers);
     //console.log('Request Body:', req.body);
     //console.log('Request:', req);
+    console.log('Request URL:', req.originalUrl);
 
     // Capture the response details
     const originalSend = res.send;
