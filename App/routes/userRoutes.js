7d3154e993079489d7bbcd124dd44b1971e99911
Charlene Lara <140762985+Grazie01@users.noreply.github.com>
2024-12-22 16:44:45 +0800

    Update userRoutes.js

--- App/routes/userRoutes.js
+++ App/routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const userRouter = express.Router();
-const path = require('path');
 const { loginUser, getUser } = require(path.resolve(__dirname,'../controllers/StudentController')); 
 
 userRouter.post('/login', loginUser);
